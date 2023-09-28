7904cc640217628e38ccf91824231d92febd3a3d
37Yash <128834519+37Yash@users.noreply.github.com>
2023-09-28 14:35:06 +0530

    uploaded

---
+++ src/dbconnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AmanTomar
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class dbconnection {
+    
+    public static Connection con;
+    
+    public static Connection getconnection()
+    {
+       
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/connecter", "root", "yashpuri1337");
+        }
+        catch(Exception e){
+            System.out.println("err: "+ e);
+        }
+         return con ;
+    }
+}
